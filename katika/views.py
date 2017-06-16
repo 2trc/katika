35aea4f98ebc903916113a9027cbb12e3bfbfdc7
2trc <tassingremi@gmail.com>
2017-06-16 15:40:39 +0200

    mezzanine version working with admin geodjango, visualization with angular and googlemaps and crispy-forms

---
+++ katika/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def busted(request):
+
+    print("formalities")
+
+    #return render(request, 'home.html', {'incidents': incidents})
+    return render(request, 'busted.html')
+
+def home(request):
+
+    #return render(request, 'home.html', {'incidents': incidents})
+    return render(request, 'home.html')
+
+
+def about(request):
+
+    return render(request, 'about.html')
+
+
