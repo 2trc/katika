3c03d0f6fd7fb7990a3ef6ec00a64e09376379b5
Remi Tassing <tassingremi@gmail.com>
2017-12-26 10:50:46 +0100

    adding blog, kthesis and polishing incident app

--- incident/urls.py
+++ incident/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
 from .views import incident_home, incident_geo_serialize,IncidentViewSet, \
-    IncidentTypeViewSet, incident_add, anycluster
+    IncidentTypeViewSet, incident_add, incident_aggregation, anycluster
 
 
 
@@ -13,7 +13,7 @@ from .views import incident_home, incident_geo_serialize,IncidentViewSet, \
 
 router = routers.DefaultRouter()
 router.register(r'api/type', IncidentTypeViewSet)
-router.register(r'api', IncidentViewSet)
+router.register(r'api', IncidentViewSet, base_name="Incident")
 
 
 # Wire up our API using automatic URL routing.
@@ -21,6 +21,7 @@ router.register(r'api', IncidentViewSet)
 urlpatterns = [
     url(r'^$', incident_home),
     url(r'^add', incident_add),
+    url(r'^aggregate', incident_aggregation),
     url(r'^', include(router.urls)),
     url(r'geojson', incident_geo_serialize),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
--- static/js/incident.js
+++ static/js/incident.js
@@ -1,6 +1,7 @@
 var incidentApp = angular.module('incidentApp', ["ngRoute"
 //  ,"angular-jquery-locationpicker",
 //  "ui.bootstrap.datetimepicker"
+    , 'daterangepicker'
 ]);
 
 //https://stackoverflow.com/questions/41211875/angularjs-1-6-0-latest-now-routes-not-working
@@ -68,19 +69,53 @@ function incidentService($http) {
 
 function IncidentCtrl(incidentService, $scope, $filter) {
 
-  console.log("Hollalalalalal");
+  initializeOrderDirection = function() {
+     $scope.isAscendingOrder = false;
+     $scope.orderIcon = "glyphicon glyphicon-arrow-down";
+
+     console.log("initializing order");
+     console.log($scope.orderIcon);
+  }
 
   var self = this;
 
-  $scope.incidentType = {};
+  $scope.incidentTypes = [];
   $scope.incident = {};
   $scope.incidents = [];
   $scope.pages = {};
+  $scope.orderType ="";
+  $scope.prevOrderType="";
+  $scope.orderIcon ="";
+  $scope.isAscendingOrder = false;
+  initializeOrderDirection();
+  $scope.incidentCount = 0;
+  $scope.woundedCount = 0;
+  $scope.deathsCount= 0;
 
-  incidentService.get('/incident/api/')
+  $scope.orderByList = ['date', 'wounded', 'deaths'];
+
+  $scope.datePicker = { 'date': {startDate: null, endDate: null} };
+
+  /*ToDo function should be renamed
+  it's called when many things change from date range,
+  to filter or order by ...
+  */
+  $scope.applyDateRange = function(ev, picker) {
+    console.log('date range applied');
+    console.log(ev);
+    console.log(picker);
+    console.log("daterange: " + JSON.stringify($scope.datePicker));
+
+    queryUrl = getQueryUrl();
+
+    url = '/incident/api?' + queryUrl;
+    console.log('url: ' + url);
+
+    incidentService.get(url)
     .then(function(incidents) {
 
       $scope.incidents = incidents.results;
+      $scope.incidentCount = incidents.count;
       $scope.pages = incidents;
       $scope.pages.results = undefined;
       console.log($scope.incidents);
@@ -88,13 +123,124 @@ function IncidentCtrl(incidentService, $scope, $filter) {
 
     });
 
-  incidentService.get('/incident/api/type/')
-    .then(function(types){
-      $scope.incidentType.types = types.results;
+    url = '/incident/aggregate?' + queryUrl;
+    incidentService.get(url)
+    .then(function(result) {
+      $scope.woundedCount = result.wounded__sum;
+      $scope.deathsCount= result.deaths__sum;
     });
 
-  $scope.gotoPage = function(pageNumber){
-    incidentService.get('/incident/api/?page='+pageNumber)
+  }
+
+  $scope.orderTypeChanged = function() {
+
+    console.log('order type change, current type');
+    console.log($scope.prevOrderType);
+
+    if( $scope.orderType == $scope.prevOrderType ){
+        return;
+    }
+    console.log('new type');
+    console.log($scope.orderType);
+
+    $scope.prevOrderType = $scope.orderType;
+
+    initializeOrderDirection();
+
+    //TODO no aggregation should be done
+    //Change name and break function
+    $scope.applyDateRange(null,null);
+  }
+
+  $scope.orderDirectionChanged = function() {
+
+    invertOrderDirection();
+    $scope.applyDateRange(null, null);
+
+  }
+
+  getQueryUrl = function() {
+
+    queryUrl = '';
+
+    startdate = $scope.datePicker.date.startDate;
+    if(startdate){
+        queryUrl += "startdate="+dateToString(startdate);
+    }
+    enddate = $scope.datePicker.date.endDate;
+    if(enddate){
+        queryUrl += "&enddate=" + dateToString(enddate);
+    }
+
+    if($scope.orderType){
+        queryUrl += "&orderby=" + $scope.orderType;
+        if($scope.isAscendingOrder){
+            queryUrl += "&order=ascending";
+        }else{
+            queryUrl += "&order=descending";
+        }
+    }
+
+    console.log($scope.typeSelected);
+
+    if($scope.typeSelected && $scope.incidentTypes.indexOf($scope.typeSelected)!=-1){
+        queryUrl += "&type="+ String($scope.typeSelected.name);
+    }
+
+    return queryUrl;
+  }
+
+  invertOrderDirection = function() {
+
+    $scope.isAscendingOrder = !$scope.isAscendingOrder;
+
+    if( $scope.isAscendingOrder ){
+        $scope.orderIcon = "glyphicon glyphicon-arrow-up";
+    }else{
+        $scope.orderIcon = "glyphicon glyphicon-arrow-down";
+    }
+
+    console.log("inverting order");
+    console.log($scope.isAscendingOrder);
+
+  }
+
+  dateToString  = function (someDate) {
+    //return String(someDate).split("T")[0];
+    //return $filter('date', someDate, 'yyyy-MM-dd');
+    return someDate.format('YYYY-MM-DD');
+  }
+
+  incidentService.get('/incident/api/')
+    .then(function(incidents) {
+
+      $scope.incidents = incidents.results;
+      $scope.incidentCount = incidents.count;
+      $scope.pages = incidents;
+      $scope.pages.results = undefined;
+      console.log($scope.incidents);
+      console.log($scope.pages);
+
+  });
+
+  incidentService.get("/incident/aggregate")
+    .then(function(result) {
+      //$scope.incidentCount = result.count;
+      $scope.woundedCount = result.wounded__sum;
+      $scope.deathsCount= result.deaths__sum;
+    });
+
+  incidentService.get('/incident/api/type/')
+    .then(function(types){
+      //$scope.incidentTypes = [{"name": "All"}].concat(types.results);
+      //[].push.apply($scope.incidentType.types, {"name": "All"});
+      $scope.incidentTypes = types.results;
+    });
+
+  $scope.gotoPage = function(pageUrl){
+    console.log("daterange: " + JSON.stringify($scope.datePicker));
+    console.log(typeof($scope.datePicker.date.startDate));
+    incidentService.get(pageUrl)
     .then(function(incidents) {
 
       $scope.incidents = incidents.results;
