b4304dd63a654703790c2749639d77feb7b39045
unknown <tassingremi@gmail.com>
2020-05-10 13:59:33 +0200

    Adding kmeans PostgreSQL extension

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,9 @@ Vagrant.configure(2) do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "ubuntu/trusty64"
+  #config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/bionic64"
+  #config.vm.box = "ubuntu/artful64"
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
@@ -22,7 +24,7 @@ Vagrant.configure(2) do |config|
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
-  config.vm.network "forwarded_port", guest: 8000, host: 8000
+  config.vm.network "forwarded_port", guest: 8000, host: 8001
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
@@ -66,6 +68,7 @@ Vagrant.configure(2) do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  config.vm.provision "shell", path: "install.sh"
+  config.vm.provision :shell, :path => "install.sh"
+  config.vm.provision :shell, :inline => "python3.6 /vagrant/manage.py runserver 0.0.0.0:8000"
 
 end
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#https://askubuntu.com/questions/865554/how-do-i-install-python-3-6-using-apt-get/865569#865569
+#sudo add-apt-repository -y ppa:deadsnakes/ppa
+
 sudo apt-get update
 
 #http://stackoverflow.com/questions/18696078/postgresql-error-when-trying-to-create-an-extension
@@ -7,16 +10,18 @@ sudo apt-get update
 
 ##?? https://stackoverflow.com/questions/16527806/cannot-create-extension-without-superuser-role
 ##?? https://stackoverflow.com/questions/18696078/postgresql-error-when-trying-to-create-an-extension
-sudo apt-get install -y postgresql-9.3 \
-						postgresql-9.3-postgis-2.1 \
+sudo apt-get install -y postgresql-9.5 \
+						postgresql-9.5-postgis-2.1 \
+						postgresql-server-dev-all \
+						pgadmin3 \
+						python3.6 \
+						python3-pip \
+						python3-psycopg2
 						#qgis \
 						#python-qgis \
 						#qgis-plugin-grass \
 						#unzip \
-						postgresql-9.3-postgis-scripts \
-						pgadmin3 \
-						python3-pip
-
+						#postgresql-9.3-postgis-scripts \
 #we are using vagrant user to deal with postgres because we have the advantage
 #of using X11 forwarding for pgAdmin and QGIS
 #X11 su user problem
@@ -25,37 +30,25 @@ sudo apt-get install -y postgresql-9.3 \
 #use postgres user to 1)create postgresql vagrant user 2)grant superuser/createdb roles
 sudo su - postgres -c "psql -c \"create user vagrant with superuser createdb;\""
 
-sudo su - vagrant -c "createdb -E UTF8 katika"
+sudo su - postgres -c "createdb -E UTF8 katika"
 
-sudo su - vagrant -c "psql katika -c \"CREATE EXTENSION postgis\""
+sudo su - postgres -c "psql katika -c \"CREATE EXTENSION postgis\""
 
-#download nyc shape file
-#if [ ! -f new_york_highway.shp ]; then
-# 	wget http://www.mapcruzin.com/download-shapefile/us/new_york_highway.zip
-# 	unzip new_york_highway.zip
-# 	sudo su - vagrant -c "shp2pgsql -c -D -s 4269 -I new_york_highway.shp |psql -d nyc"
-#fi
+#sudo su - postgres -c "psql -c \"create user lapiro password 'password'\""
 
-#http://www.mapcruzin.com/free-united-states-shapefiles/free-new-york-arcgis-maps-shapefiles.htm
-#importing shape file
-#http://postgis.net/docs/manual-2.2/using_postgis_dbmanagement.html#loading_geometry_data
+# https://anycluster.readthedocs.io/en/latest/installconf.html
+git clone https://github.com/umitanuki/kmeans-postgresql.git
+
+cd kmeans-postgresql
+make
+sudo make install
+
+sudo su - postgres -c "psql -f /usr/share/postgresql/10/extension/kmeans.sql -d katika"
+
+sudo pip3 install -r /vagrant/requirements.txt
+
+# https://code.djangoproject.com/ticket/20036
+# django.contrib.gis.geos.error.GEOSException: Could not parse version info string "3.4.0dev-CAPI-1.8.0 r0"
+# TODO edit /usr/local/lib/python3.6/dist-packages/django/contrib/gis/geos/libgeos.py
 
 
-#sample queries
-#
-#select ST_AsText(geom),* from new_york_highway limit 20;
-#select type, count(type) as num from new_york_highway group by type;
-
-#Find intersection in a polygon/triangle somewhere around NYC-Manhattan
-#select name, type,oneway
-#from new_york_highway 
-#where ST_Intersects(geom, ST_GeomFromText('POLYGON((-73.968890 40.769531, -73.960991 40.767752, -73.965609 40.764638, -73.968890 40.769531))', 4269))
-#limit 20;
-
-sudo pip install virtualenv
-
-virtualenv venv -p /usr/bin/python3
-
-source venv/bin/activate
-
-pip install -r requirements.txt
