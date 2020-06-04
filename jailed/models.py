7bda356c6d3397eb908cdbe885584ec8be89feb4
unknown <tassingremi@gmail.com>
2020-06-04 07:39:29 +0200

    jailed with pagination, prison location, search, sorting

--- jailed/models.py
+++ jailed/models.py
@@ -1,25 +1,48 @@
 from django.db import models
+from katika.models import NullsLastQuerySet
 from django.contrib import admin
 from person.models import Person, SEX
+from django.contrib.gis.db import models as geo_models
+from incident.models import Tag
+from mapwidgets.widgets import GooglePointFieldWidget
 
 # Create your models here.
 
+class IncarcerationTag(Tag):
+    pass
+
+
+class Prison(models.Model):
+    name = models.CharField(max_length=255)
+    short_name = models.CharField(max_length=30, blank=True, null=True)
+    location = geo_models.PointField()
+
+    def __str__(self):
+        return self.short_name if self.short_name else self.name
+
+
 class Incarceration(Person):
-    #prison = 
     arrest_date = models.DateField(null=True, blank=True)
-    #arrest_place =,
     incarceration_date = models.DateField(null=True, blank=True)
     conviction_date = models.DateField(null=True, blank=True)
     conviction_duration = models.DurationField(null=True, blank=True)
     release_date = models.DateField(null=True, blank=True)
+    prison = models.ForeignKey(Prison, null=True, blank=True, on_delete=models.SET_NULL)
     sources = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField(IncarcerationTag, blank=True)
+
+    #https://stackoverflow.com/questions/15121093/django-adding-nulls-last-to-query
+    objects = NullsLastQuerySet.as_manager()
     
     def more_info(self):
 
         m_info = "Birthday: {}, ".format(self.birthday) if self.birthday else ""
         
-        if self.sex:
-            m_info += "Sex: {}".format(SEX[self.sex])
+        if self.sex is not None:           
+            if SEX[self.sex][1] == 'M':
+                m_info += "Male, "
+            else:
+                m_info += "Female"
 
         if self.sources:
             m_info += "\nSources: {}".format(self.sources)
@@ -30,4 +53,26 @@ class Incarceration(Person):
     #class Meta:
     #    managed = True
 
-admin.site.register(Incarceration)
+
+class IncarcerationAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name', 'prison', 'arrest_date', 'incarceration_date', 'conviction_date', 'release_date')
+    search_fields = ('first_name', 'last_name',)
+    #list_filter = ('type', 'date')
+
+
+admin.site.register(Incarceration, IncarcerationAdmin)
+
+class PrisonAdmin(admin.ModelAdmin):
+
+    fiels = ["name", "short_name", "location"]
+
+    formfield_overrides = {
+        geo_models.PointField: {"widget": GooglePointFieldWidget},
+        # KeywordsField: {"widget": KeywordsWidget},
+
+    }
+
+
+admin.site.register(IncarcerationTag)
+admin.site.register(Prison, PrisonAdmin)
--- jailed/views.py
+++ jailed/views.py
@@ -1,21 +1,46 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.contrib.postgres.search import SearchVector
 from .models import Incarceration
 
-# Create your views here.
-def jailed_home(request):
 
-    jailed_set = Incarceration.objects.all().order_by(
-        '-arrest_date', '-incarceration_date', '-conviction_date', '-release_date')
+def jailed_home(request):
+    # &q={{q}}&o={{curr.o}}&s={{curr.s}}
+    # q={{q}}&o=name&s={{s.name}}, s.arrest, s.incarceration, s.conviction, s.release, s.prison
+    curr = {'o': request.GET.get('o'), 's': request.GET.get('s')}
+    s = {'last_name': None, 'arrest': None, 'incarceration': None, 'conviction': None, 'release': None, 'prison': None}
+
+    order_list = []
+
+    for k in s:
+
+        print(k,curr)
+
+        if k == curr['o']:
+
+            order_str = "{}{}".format(get_sign(curr['s']), k)
+            s[k] = 'dsc' if curr['s'] != 'dsc' else 'asc'
+
+            if k not in ('last_name', 'prison'):
+                order_str += "_date"
+
+            order_list.append(order_str)
+
+    if len(order_list) == 0:
+        order_list = ['-arrest_date', '-incarceration_date', '-conviction_date', '-release_date']
+
+    q_str = request.GET.get('q', '')
+
+    if q_str:
+        jailed_set = Incarceration.objects.annotate(
+            search=SearchVector('last_name', 'first_name'),
+        ).filter(search=q_str)
+    else:
+        jailed_set = Incarceration.objects.all()
+
+    jailed_set = jailed_set.order_by(*order_list)
 
     page = request.GET.get('page', 1)
-
-    print(request.GET)
-
-    #query_str = request.GET.get('q')
-
-    #if(query_str):
-    #    jailed_set = perform_thesis_search(jailed_set, query_str)
 
     paginator = Paginator(jailed_set, 50)
 
@@ -27,4 +52,9 @@ def jailed_home(request):
     except EmptyPage:
         jailed = paginator.page(paginator.num_pages)
 
-    return render(request, 'jailed.html', context= {'jailed': jailed})
+    return render(request, 'jailed.html', context= {'jailed': jailed, 'paginator': paginator,
+                                                    's': s, 'curr': curr, 'q': q_str})
+
+
+def get_sign(sign_name):
+    return "" if sign_name == 'dsc' else "-"
