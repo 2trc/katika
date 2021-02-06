dc9873316f543477fba7623e5b146421fe7efb0c
remi tassing <tassingremi@gmail.com>
2021-02-06 22:48:17 +0100

    automated vagrant startup

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-sudo apt-get update
+#https://askubuntu.com/questions/865554/how-do-i-install-python-3-6-using-apt-get/865569#865569
+#sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+apt-get update
 
 #http://stackoverflow.com/questions/18696078/postgresql-error-when-trying-to-create-an-extension
 #https://www.qgis.org/en/site/forusers/alldownloads.html#debian-ubuntu
 
 ##?? https://stackoverflow.com/questions/16527806/cannot-create-extension-without-superuser-role
 ##?? https://stackoverflow.com/questions/18696078/postgresql-error-when-trying-to-create-an-extension
-sudo apt-get install -y postgresql-9.3 \
-						postgresql-9.3-postgis-2.1 \
-						#qgis \
-						#python-qgis \
-						#qgis-plugin-grass \
-						#unzip \
-						postgresql-9.3-postgis-scripts \
+apt-get install -y postgresql-10 \
+						postgresql-10-postgis-2.4 \
+						postgresql-server-dev-all \
 						pgadmin3 \
-						python3-pip
+						python3.6 \
+						python3-pip \
+						python3-psycopg2
 
 #we are using vagrant user to deal with postgres because we have the advantage
 #of using X11 forwarding for pgAdmin and QGIS
@@ -23,39 +24,34 @@ sudo apt-get install -y postgresql-9.3 \
 #http://unix.stackexchange.com/questions/110558/su-with-error-x11-connection-rejected-because-of-wrong-authentication
 
 #use postgres user to 1)create postgresql vagrant user 2)grant superuser/createdb roles
-sudo su - postgres -c "psql -c \"create user vagrant with superuser createdb;\""
+su - postgres -c "psql -c \"create user vagrant with superuser createdb;\""
 
-sudo su - vagrant -c "createdb -E UTF8 katika"
+su - vagrant -c 'createdb -E UTF8 katika'
 
-sudo su - vagrant -c "psql katika -c \"CREATE EXTENSION postgis\""
+su - vagrant -c "psql katika -c \"CREATE EXTENSION postgis\""
 
-#download nyc shape file
-#if [ ! -f new_york_highway.shp ]; then
-# 	wget http://www.mapcruzin.com/download-shapefile/us/new_york_highway.zip
-# 	unzip new_york_highway.zip
-# 	sudo su - vagrant -c "shp2pgsql -c -D -s 4269 -I new_york_highway.shp |psql -d nyc"
-#fi
+cd /tmp
+# https://anycluster.readthedocs.io/en/latest/installconf.html
+git clone https://github.com/umitanuki/kmeans-postgresql.git
 
-#http://www.mapcruzin.com/free-united-states-shapefiles/free-new-york-arcgis-maps-shapefiles.htm
-#importing shape file
-#http://postgis.net/docs/manual-2.2/using_postgis_dbmanagement.html#loading_geometry_data
+cd kmeans-postgresql
+make
+make install
 
+su - vagrant -c "psql -f /usr/share/postgresql/10/extension/kmeans.sql -d katika"
 
-#sample queries
-#
-#select ST_AsText(geom),* from new_york_highway limit 20;
-#select type, count(type) as num from new_york_highway group by type;
+pip3 install -r /vagrant/requirements.txt
 
-#Find intersection in a polygon/triangle somewhere around NYC-Manhattan
-#select name, type,oneway
-#from new_york_highway 
-#where ST_Intersects(geom, ST_GeomFromText('POLYGON((-73.968890 40.769531, -73.960991 40.767752, -73.965609 40.764638, -73.968890 40.769531))', 4269))
-#limit 20;
+# https://code.djangoproject.com/ticket/20036
+# django.contrib.gis.geos.error.GEOSException: Could not parse version info string "3.4.0dev-CAPI-1.8.0 r0"
+# https://unix.stackexchange.com/questions/32907/what-characters-do-i-need-to-escape-when-using-sed-in-a-sh-script
+sed -i 's/( r\\d.*/.*$'\''/' /usr/local/lib/python3.6/dist-packages/django/contrib/gis/geos/libgeos.py
+#version_regex = re.compile(
+#    r'^(?P<version>(?P<major>\d+)\.(?P<minor>\d+)\.(?P<subminor>\d+))'
+#    r'((rc(?P<release_candidate>\d+))|dev)?-CAPI-(?P<capi_version>\d+\.\d+\.\d+).*$'
+#)
 
-sudo pip install virtualenv
+su - vagrant -c "whoami && pwd"
+su - vagrant -c "cd /vagrant && python3 manage.py makemigrations && python3 manage.py migrate"
+su - vagrant -c "nohup python3.6 /vagrant/manage.py runserver 0.0.0.0:8000 &"
 
-virtualenv venv -p /usr/bin/python3
-
-source venv/bin/activate
-
-pip install -r requirements.txt
