b8068d2d2708bcebfa369c166d0fba1d62467c60
remi tassing <tassingremi@gmail.com>
2021-11-12 22:35:53 +0100

    add tender commands for collecting content, new entries, convert region EN to FR

---
+++ tender/management/commands/regions_english_to_french.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand, CommandError
+from tender.models import ArmpEntry
+
+
+# TODO: logging in file and std.ou?
+# TODO: testing
+
+# ref: https://docs.djangoproject.com/en/dev/howto/custom-management-commands/
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        region_list = {
+            "WEST": "OUEST" ,
+            "EAST": "EST",
+            "CENTER": "CENTRE",
+            "NORTH-WEST": "NORD-OUEST",
+            "SOUTH-WEST": "SUD-OUEST",
+            "FAR-NORTH": "EXTRÊME-NORD",
+            "NORTH": "NORD",
+            "ADAMAWA": "ADAMAOUA",
+            "SOUTH": "SUD",
+            "CENTRAL SERVICES": "SERVICES CENTRAUX"
+        }
+
+        for r_en, r_fr in region_list.items():
+
+            ArmpEntry.objects.filter(region=r_en).update(region=r_fr)
+
