fa2479d77d5b986e4731f62787b0cdf59e460937
2trc <tassingremi@gmail.com>
2022-01-16 17:15:54 +0100

    Find new Owners and Insert them

--- tender/daily_tender_fetcher.sh
+++ tender/daily_tender_fetcher.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd /opt/katika
-source venv/bin/active
-python manage.py get_armp_entries
-python manage.py collect_content_when_empty
+#source venv/bin/active
+venv/bin/python manage.py get_armp_entries
+venv/bin/python manage.py collect_content_when_empty
+venv/bin/python manage.py tender_twitter_bot
