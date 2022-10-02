7ca6afc5f9a704afd1d9a6e650eba37800bc34c0
2trc <tassingremi@gmail.com>
2022-10-02 06:20:38 +0200

    DEBUG=True and SECRET_KEY to settings.py for easy first-timer setup

--- katika/settings.py
+++ katika/settings.py
@@ -120,7 +120,8 @@ LANGUAGES = (
 # A boolean that turns on/off debug mode. When set to ``True``, stack traces
 # are displayed for error pages. Should always be set to ``False`` in
 # production. Best set to ``True`` in local_settings.py
-DEBUG = False
+DEBUG = True
+SECRET_KEY = "asdasd12312!@^!^#$!@#$!^@#^!$@"
 
 # Whether a user's session cookie expires when the Web browser is closed.
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
