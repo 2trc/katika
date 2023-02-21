ec9582f765070bb5e7797f6b89ca33a0f85969c0
2trc <tassingremi@gmail.com>
2023-02-21 07:03:41 +0100

    Allow users to Update Loi de Finances

--- budget/urls.py
+++ budget/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.budget_global),
+    url(r'^add-budget-programme', views.budgetprogramme_add),
     url(r'^region', views.region),
     url(r'^prog$', views.budget_programme),
     ]
