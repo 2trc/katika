39bc347128726c2fd4b230ba5ec23098c1408e83
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-05-19 18:55:24 +0200

    wip : getting katika running on my server

--- katika/settings.py
+++ katika/settings.py
@@ -457,8 +457,8 @@ LOGIN_URL = 'login'
 # List of middleware classes to use. Order is important; in the request phase,
 # these middleware classes will be applied in the order given, and in the
 # response phase the middleware will be applied in reverse order.
-MIDDLEWARE_CLASSES = (
-#MIDDLEWARE = (
+
+MIDDLEWARE = (
     "mezzanine.core.middleware.UpdateCacheMiddleware",
 
     'django.contrib.sessions.middleware.SessionMiddleware',
