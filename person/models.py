af2f4cd438810254426fd17ac2dcdc65138e77e5
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-05-19 19:58:11 +0200

    wip: fixed deprecated usages

--- person/models.py
+++ person/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from mezzanine.core.fields import FileField
 from mezzanine.utils.models import upload_to
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
