bd4441d1373dbc5a8f70f55789de52cbf14058f6
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-05-20 10:57:00 +0200

    fixed admin password_reset view name missing

--- katika/urls.py
+++ katika/urls.py
@@ -26,7 +26,7 @@ urlpatterns = i18n_patterns(
     # Change the admin prefix here to use an alternate URL for the
     # admin interface, which would be marginally more secure.
     path("admin/", include(admin.site.urls)),
-    path("admin/password_reset/", auth_views.PasswordResetView.as_view(), name="admin_password_reset"),
+    path("admin/password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
     path("admin/password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset/done/", auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
